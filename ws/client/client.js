1bfeafdcf72d4b5e1c689c4347c3dd705b9f6dae
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-05-18 17:11:02 +0530

    final addition

--- ws/client/client.js
+++ ws/client/client.js
@@ -5,6 +5,7 @@ let serverSend = document.querySelector(".server-send");
 let mediaContainer = document.querySelector(".container");
 let fileSend = document.getElementById("file-button");
 let playMusic = document.getElementById("play-music");
+let anchorTag = document.querySelector("a"); 
 send.disabled = true;
 fileInput.disabled = true;
 fileSend.style.display = "none";
@@ -20,16 +21,22 @@ input.addEventListener("keypress", (e) => {
 const socket = new WebSocket("ws://localhost:5500");
 
 socket.onopen = (e) => {
-  console.log();
   console.log("Client socket opened!");
   send.disabled = false;
   fileInput.disabled = false;
 
-  const name = prompt("Enter Chat username: ");
+  let name = prompt("Enter Chat username: ");
+  if (name == "") {
+    alert("Username is required!");
+    while(name == "") {
+      name = prompt("Enter chat username")
+    }
+  }
   socket.send(name);
 };
 
 socket.onmessage = (message) => {
+  scroll()
   const { data } = message;
   let mediaSrc = data.split(" ")[1];
   let mediaType = data.split(";")[0].split(":")[2];
@@ -68,6 +75,13 @@ socket.onmessage = (message) => {
       music.play();
     };
     return;
+  } else if (mediaType == "application/pdf") {
+    const download = document.createElement("button"); 
+    download.innerText = "Download PDF";
+    anchorTag.href = mediaSrc;
+    anchorTag.download = "";
+    anchorTag.appendChild(download);
+    return
   }
 
   serverSend.innerHTML += `
@@ -79,6 +93,7 @@ socket.onmessage = (message) => {
 send.addEventListener("click", sendMessage, false);
 
 function sendMessage() {
+  scroll()
   let message = input.value;
   socket.send(message);
   serverSend.innerHTML += `
@@ -108,3 +123,10 @@ fileInput.addEventListener("change", () => {
       </span>`;
   };
 });
+
+function scroll () {
+  serverSend.scrollTo({
+    top: serverSend.scrollHeight + 400, 
+    behavior: "smooth"
+  })
+}
